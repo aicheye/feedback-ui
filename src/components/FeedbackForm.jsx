b8392d91a045ebbe9ac03acfc57c8c608f025dc7
aicheye <seanxxyang@gmail.com>
2024-06-07 22:49:34 -0400

    Cleanup

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -56,7 +56,7 @@ function FeedbackForm() {
   return (
     <Card>
       <form onSubmit={handleSubmit}>
-        <h2>How would you rate your service with us?</h2>
+        <h2>How would you rate your experience with us today?</h2>
         <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
           <input
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -10,9 +10,28 @@ function AboutPage() {
           This project is a simple feedback form that allows users to submit
           feedback and view feedback statistics.
         </p>
-        <p>The front end is built with pure React and Framer Motion.</p>
-        <p>The back end is built with Firebase and Firestore.</p>
-        <p>Version: 1.0.0</p>
+        <p>
+          The frontend is built with Javascript, NodeJS, React 18, and Framer
+          Motion. The backend is on Google Firebase and Cloud Firestore. It is
+          currently set to read-only for safety reasons.
+        </p>
+        <p>
+          This project was made as part of Brad Traversy's React Front To Back
+          course on{" "}
+          <a
+            href="https://www.udemy.com/course/react-front-to-back-2022"
+            rel="noreferrer"
+          >
+            Udemy
+          </a>
+          .
+        </p>
+        <p>
+          Version:{" "}
+          <a href="https://github.com/aicheye/feedback-ui" rel="noreferrer">
+            1.0.0
+          </a>
+        </p>
 
         <p>
           <Link to="/">Back to Home</Link>
